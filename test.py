7c78e0aa82e99fc6ac6a86a6aa32265a597352e3
Luís Vieira <lpedro.vieira18@gmail.com>
2023-12-10 16:24:22 +0000

    alter test.py

--- test.py
+++ test.py
@@ -12,46 +12,22 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-print(device)
-
-environment = DQNBreakout(device = device, render_mode='human')
+input_file = 'latest.pt'
 
 model = AtariNet(nb_actions=4)
 
-model.to(device)
-
-model.load_the_model(weights_filename='latest.pt')
+model.load_the_model(weights_filename=input_file)
 
 agent = Agent(model=model,
               device=device,
               epsilon=0.05,
-              nb_warmup=5000,
+              nb_warmup=50,
               nb_actions=4,
-              learning_rate=0.00001,
               memory_capacity=1000000,
               batch_size=64)
 
-agent.test(env=environment)
+test_environment = DQNBreakout(device = device, render_mode='human')
 
+agent.test(env=test_environment)
 
-
-'''
-state = environment.reset()
-
-action_probs = model.forward(state).detach()
-print(f"{action_probs}, {torch.agrmax(action_probs, dim=-1, keepdim=True)}")
-'''
-
-'''
-for i in range(10):
-    environment.reset()
-    done = False
-    while not done:
-        action = environment.action_space.sample()
-        state, reward, done, info = environment.step(action)
-        environment.render()
-    
-    print(state.shape)
-
-'''
 
